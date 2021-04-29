16e41cbaa251e453966f20f53c9c2866af226b98
Maria Patokina <mpatokina@hotmail.com>
2021-04-29 12:56:29 -0400

    week 4 Lists

--- hello.py
+++ hello.py
@@ -507,7 +507,224 @@ def replace_ending(sentence, old, new):
     return sentence
 print(replace_ending("it's raining cats and cats", "cats", "dogs")) 
 print(replace_ending("I have a cat", "Cat", "Dog"))
-print(replace_ending("I have a Cat", "Cat", "Dog"))            
-        
+print(replace_ending("I have a Cat", "Cat", "Dog"))   
+
+def string_repeats_letters(string):
+    for i in range (0, len(string) - 1):
+        for j in range (i+1, len(string)):
+            if string[i] == string[j]:
+                return True
+    return False
+print(string_repeats_letters("hello"))
+print(string_repeats_letters("bye")) 
+print(string_repeats_letters("abcdefghijklmnopqrstuvwxyz"))
+print(string_repeats_letters("abcdefga"))      
+
+for i in range (1, 100):
+    if (i % 3 == 0) and (i % 5 == 0):
+        print("buzzfuzz")
+    elif (i % 3 == 0):
+        print("buzz")
+    elif (i % 5 == 0):
+        print("fuzz")
+    else:
+        print(i)
+
+def is_leap(year):
+    leap = False
+    if (year % 400 == 0):
+        leap = True
+    elif (year % 100 == 0):
+        leap = False
+    elif (year % 4 == 0):
+        leap = True
+    print(leap)
+is_leap(1900) 
+is_leap(2000)  
+is_leap(1700) 
+is_leap(2000)
+is_leap(2021)     
+
+#Lists
+x = ["Now", "we", "are", "cooking!"]
+print(type(x))
+print(x)
+print(len(x))
+print(len(x[3]))
+print("are" in x)
+print("you" in x)
+print(x[-1])
+print(x[0])
+print(x[1:3])
+print(x[:2])
+print(x[2:])
+
+def get_word(sentence, n):
+    if n > 0:
+        word = sentence.split()
+        if n <= len(word):
+            return (word[n-1])
+    return ("")
+print(get_word("Now we are cooking!", 4)) 
+print(get_word("Now we are cooking!", 5)) 
+print(get_word("Now we are cooking!", 0))    
 
 
+fruits = ["Pineapple", "Banana", "Apple"]
+fruits.append("Kiwi")
+print(fruits)
+fruits.insert(0, "Orange")
+print(fruits)
+fruits.remove("Apple")
+print(fruits)
+fruits.pop(1)
+print(fruits)
+fruits[1] = "Strawberry"
+print(fruits)
+
+def skip_elements(elements):
+    new_list = []
+    for i in range (len(elements)):
+        if (i % 2 == 0):
+            new_list.append(elements[i])
+        i += 1
+    return new_list
+print(skip_elements(["a", "b", "c", "d", "e", "f"])) 
+print(skip_elements([]))    
+
+# Iterating over Lists and Tuples
+animals = ["Lion", "Zebra", "Dolphin", "Monkey"]
+chars = 0
+for animal in animals:
+    chars += len(animal)
+print("Total characters: {}, Average length: {}".format(chars, chars/len(animals)))
+
+def skip_elements2(elements):
+    new_elements = []
+    for index, element in enumerate(elements):
+        if (index % 2 == 0):
+            new_elements.append(element)
+    return new_elements
+print(skip_elements2(["a", "b", "c", "d", "e", "f"]))       
+
+#List Comprehensions
+multiples = []
+for x in range(1, 11):
+    multiples.append(x*7)
+print(multiples)  
+
+multiples2 = [x*7 for x in range(1, 11)]
+print(multiples2)
+
+languages = ["Python", "Perl", "Ruby", "Go", "Java", "C"]
+lengths = [len(language) for language in languages]
+print(lengths)
+
+z = [x for x in range(0,101) if x % 3 == 0]
+print(z)
+
+def odd_numbers(n):
+    return [x for x in range(0, n+1) if x % 2 != 0]
+print(odd_numbers(17))   
+
+
+#Practice Quiz: Lists (Week 4)
+#Question 1
+#Given a list of filenames, we want to rename all the files with extension hpp to the extension h. To do this, we would like to generate a new list called newfilenames, consisting of the new filenames. Fill in the blanks in the code using any of the methods you’ve learned thus far, like a for loop or a list comprehension.
+filenames = ["program.c", "stdio.hpp", "sample.hpp", "a.out", "math.hpp", "hpp.out"]
+# Generate newfilenames as a list containing the new filenames
+# using as many lines of code as your chosen method requires.
+
+new_filenames = []
+for filename in filenames:
+    if filename.endswith(".hpp"):
+        l = len(filename) - 4
+        filename = filename[:l] + ".h" 
+        new_filenames.append(filename)
+    else:
+        new_filenames.append(filename)     
+
+print(new_filenames) 
+# Should be ["program.c", "stdio.h", "sample.h", "a.out", "math.h", "hpp.out"]
+
+
+#Question 2
+#Let's create a function that turns text into pig latin: a simple text transformation that modifies each word moving the first character to the end and appending "ay" to the end. For example, python ends up as ythonpay.
+def pig_latin(text):
+  say = ""
+  # Separate the text into words
+  words = text.split()
+  for word in words:
+    # Create the pig latin word and add it to the list
+    new_word = word[1:] + word[0] + "ay "
+    # Turn the list back into a phrase
+    say += new_word
+  return say
+		
+print(pig_latin("hello how are you")) # Should be "ellohay owhay reaay ouyay"
+print(pig_latin("programming in python is fun")) # Should be "rogrammingpay niay ythonpay siay unfay"
+
+
+
+#Question 3
+#The permissions of a file in a Linux system are split into three sets of three permissions: read, write, and execute for the owner, group, and others. Each of the three values can be expressed as an octal number summing each permission, with 4 corresponding to read, 2 to write, and 1 to execute. Or it can be written with a string using the letters r, w, and x or - when the permission is not granted.
+#For example: 
+#640 is read/write for the owner, read for the group, and no permissions for the others; converted to a string, it would be: "rw-r-----"
+#755 is read/write/execute for the owner, and read/execute for group and others; converted to a string, it would be: "rwxr-xr-x"
+#Fill in the blanks to make the code convert a permission in octal format into a string format.
+
+def octal_to_string(octal):
+    result = ""
+    value_letters = [(4,"r"),(2,"w"),(1,"x")]
+    # Iterate over each of the digits in octal
+    for digit in [int(n) for n in str(octal)]:
+        # Check for each of the permissions values
+        for value, letter in value_letters:
+            if digit >= value:
+                result += letter
+                digit -= value
+                
+            else:
+                result += "-"
+                
+    return result
+    
+print(octal_to_string(755)) # Should be rwxr-xr-x
+print(octal_to_string(644)) # Should be rw-r--r--
+print(octal_to_string(750)) # Should be rwxr-x---
+print(octal_to_string(600)) # Should be rw-------
+print(octal_to_string(777)) # Should be rwxrwxrwx
+print(octal_to_string(111)) # Should be --x--x--x
+print(octal_to_string(222)) # Should be -w--w--w-
+print(octal_to_string(444)) # Should be r--r--r--
+
+# Question 5
+# The group_list function accepts a group name and a list of members, and returns a string with the format: group_name: member1, member2, … For example, group_list("g", ["a","b","c"]) returns "g: a, b, c". Fill in the gaps in this function to do that.
+def group_list(group, users):
+  members = ""
+  l=0
+  for user in users:
+    members += user + ", "
+    l=len(members)
+  return "{}: {}".format(group, members[0:l-2])
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
+
+# Question 6
+#The guest_list function reads in a list of tuples with the name, age, and profession of each party guest, and prints the sentence "Guest is X years old and works as __." for each one. For example, guest_list(('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")) should print out: Ken is 30 years old and works as Chef. Pat is 35 years old and works as Lawyer. Amanda is 25 years old and works as Engineer. Fill in the gaps in this function to do that. 
+def guest_list(guests):
+	for guest in guests:
+		name, age, job = guest
+		print("{} is {} years old and works as {}".format(name, age, job))
+
+guest_list([('Ken', 30, "Chef"), ("Pat", 35, 'Lawyer'), ('Amanda', 25, "Engineer")])
+
+#Click Run to submit code
+"""
+Output should match:
+Ken is 30 years old and works as Chef
+Pat is 35 years old and works as Lawyer
+Amanda is 25 years old and works as Engineer
+"""
