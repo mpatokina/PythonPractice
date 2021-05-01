e187741676ac8e51fc058245fc37b841cccdbece
Maria Patokina <mpatokina@hotmail.com>
2021-04-30 23:47:40 -0400

    Week4 Dictionaries

--- hello.py
+++ hello.py
@@ -728,3 +728,101 @@ Ken is 30 years old and works as Chef
 Pat is 35 years old and works as Lawyer
 Amanda is 25 years old and works as Engineer
 """
+
+# Dictionaries
+
+x = {}
+print(type(x))
+
+
+# The "toc" dictionary represents the table of contents for a book. Fill in the blanks to do the following: 	# 
+# 1) Add an entry for Epilogue on page 39. 	2) Change the page number for Chapter 3 to 24. 	
+# 3) Display the new dictionary contents. 	
+# # 4) Display True if there is Chapter 5, False if there isn't.
+
+toc = {"Introduction":1, "Chapter 1":4, "Chapter 2":11, "Chapter 3":25, "Chapter 4":30}
+toc["Epilogue"] = 39
+toc["Chapter 3"] = 24
+del toc["Introduction"]
+print(toc)
+print("Chapter 5" in toc)
+
+# Iterating over the contents of a dictionary
+
+file_counts = {"jpg":10, "txt":14, "csv":2, "py":23}
+for extention in file_counts:
+    print(extention)
+
+for value in file_counts.values():
+    print(value)    
+
+for ext, amount in file_counts.items(): # items method
+    print("There are {} files with the.{} extention.".format(amount, ext))   
+
+print(file_counts.keys())   
+
+print(file_counts.values())
+
+cool_beasts = {"octopuses":"tentacles", "dolphins":"fins", "rhinos":"horns"}
+for ext, amount in cool_beasts.items():
+    print("{} have {}".format(ext, amount))
+
+def count_letters(text):
+    result = {}
+    for letter in text:
+        if letter not in result:
+            result[letter] = 0
+        result[letter] += 1
+    return result
+print(count_letters("hello, how are you?"))            
+        
+wardrobe = {"shirt":["red", "blue", "white"], "jeans":["blue", "black"]}
+for outfit, colors in wardrobe.items():
+    for color in colors:
+        print("{} {}".format(color, outfit))
+
+# 1
+# The email_list function receives a dictionary, which contains domain names as keys, and a list of users as values. Fill in the blanks to generate a list that contains complete email addresses (e.g. diana.prince@gmail.com).
+
+def email_list(domains):
+    emails = []
+    for domain, names in domains.items():
+        for name in names:
+            emails.append("{}@{}".format(name, domain))
+    return emails    
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+
+#5
+# The add_prices function returns the total price of all of the groceries in the  dictionary. Fill in the blanks to complete this function.
+
+def add_prices(basket):
+    total = 0
+    for value in basket.values():
+        total += value
+    return round(total, 2)
+
+groceries = {"bananas": 1.56, "apples": 2.50, "oranges": 0.99, "bread": 4.59, 
+	"coffee": 6.99, "milk": 3.39, "eggs": 2.98, "cheese": 5.44}
+
+print(add_prices(groceries)) # Should print 28.44
+
+# 2. The groups_per_user function receives a dictionary, which contains group names with the list of users. Users can belong to multiple groups. Fill in the blanks to return a dictionary with the users as keys and a list of their groups as values. 
+
+def groups_per_user(group_dictionary):
+	user_groups = {}
+	# Go through group_dictionary
+	for group, users in group_dictionary.items():
+		# Now go through the users in the group
+		for user in users:
+			if user not in user_groups:
+				user_groups[user] = []
+			user_groups[user].append(group)	
+			# Now add the group to the the list of
+# groups for this user, creating the entry
+# in the dictionary if necessary
+
+	return(user_groups)
+
+print(groups_per_user({"local": ["admin", "userA"],
+		"public":  ["admin", "userB"],
+		"administrator": ["admin"] }))
